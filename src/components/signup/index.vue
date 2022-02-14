86828ef2dbb505fb0ac705c160fd6e8465eae8f3
Preeti <singhpreeti236@gmail.com>
2022-02-14 06:29:34 -0500

    hide password

--- src/components/signup/index.vue
+++ src/components/signup/index.vue
@@ -24,6 +24,7 @@
                 ></v-text-field>
                 <v-text-field
                     v-model="password"
+                    type="password"
                     :rules="[(v) => !!v || 'Password is required']"
                     label="Password"
                     required
@@ -37,7 +38,6 @@
         <div v-else>
             <v-card>
                 <v-card-title>User Submitted successfully!</v-card-title>
-
                 <v-card-subtitle>Click the button to add new Dataset.</v-card-subtitle>
                 <v-btn to="/addDatasets" block elevation="2">Add new datasets</v-btn>
                 <v-btn to="/datasetsList" block elevation="2">View your registered datasets</v-btn>
